fea9fe4e17f84c1fe0eab9926ad807c0d7b68d32
Konstantin Belousov <bks79@icloud.com>
2019-03-10 11:58:27 +0300

    заменил устаревший адаптер pager view на более новый

--- app/src/main/java/com/starikov/getweather/MainActivity.java
+++ app/src/main/java/com/starikov/getweather/MainActivity.java
@@ -8,18 +8,24 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import com.starikov.getweather.adapters.WeatherPagerAdapter;
+
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
-    private static final int PAGER_COUNT = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         ViewPager viewPager = findViewById(R.id.view_pager);
-        WeatherPagerAdapter pagerAdapter = new WeatherPagerAdapter(getSupportFragmentManager(), PAGER_COUNT);
+        WeatherPagerAdapter pagerAdapter = new WeatherPagerAdapter(
+                Arrays.asList(new CurrentWeatherFragment(), new WeatherForecastFragment()),
+                getSupportFragmentManager());
         viewPager.setAdapter(pagerAdapter);
     }
 
---
+++ app/src/main/java/com/starikov/getweather/adapters/WeatherPagerAdapter.java
@@ -0,0 +1,27 @@
+package com.starikov.getweather.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.List;
+
+public class WeatherPagerAdapter extends FragmentStatePagerAdapter {
+
+    private List<Fragment> fragments;
+
+    public WeatherPagerAdapter(List<Fragment> fragments, FragmentManager fragmentManager) {
+        super(fragmentManager);
+        this.fragments = fragments;
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return fragments.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+}
